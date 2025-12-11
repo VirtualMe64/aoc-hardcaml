03a1c5cb8a56bf3ac2624f3614a5f7e543d23937
VirtualMe64 <staubman1@gmail.com>
2025-12-10 21:19:34 -0500

    Initial file structure

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,6 @@
-let () = print_endline "Hello, World!"
+open Hardcaml
+
+module CounterCircuit = Circuit.With_interface(Counter.I)(Counter.O)
+let circuit = CounterCircuit.create_exn Counter.create ~name:"counter"
+
+let () = Rtl.print Verilog circuit
---
+++ test/counter/test_aoc_hardcaml.ml
@@ -0,0 +1,34 @@
+(* https://github.com/janestreet/hardcaml/blob/master/docs/counter_example.md *)
+open Hardcaml
+open Counter
+
+module Simulator = Cyclesim.With_interface(I)(O)
+
+let%expect_test "testbench" =
+    (* Construct the simulation and get its input and output ports. *)
+    let sim = Simulator.create create in
+    let inputs : _ I.t = Cyclesim.inputs sim in
+    let outputs : _ O.t = Cyclesim.outputs sim in
+    (* Perform a clock cycle.  Apply the given values to [incr] and [clear].
+       Printf the current values of [dout]. *)
+    let step ~clear ~incr =
+      inputs.clear := if clear=1 then Bits.vdd else Bits.gnd;
+      inputs.incr := if incr=1 then Bits.vdd else Bits.gnd;
+      Stdio.printf "dout='%s'\n" (Bits.to_string !(outputs.dout));
+      Cyclesim.cycle sim
+    in
+    (* Run the counter for 6 clock cycles. *)
+    step ~clear:0 ~incr:0;
+    step ~clear:0 ~incr:1;
+    step ~clear:0 ~incr:1;
+    step ~clear:1 ~incr:0;
+    step ~clear:0 ~incr:0;
+    step ~clear:0 ~incr:0;
+    [%expect {|
+      dout='00000000'
+      dout='00000000'
+      dout='00000001'
+      dout='00000010'
+      dout='00000000'
+      dout='00000000'
+      |}]
