27310e7e722503a75f7b351b30e49c9ba534c3af
VirtualMe64 <staubman1@gmail.com>
2025-12-11 18:21:27 -0500

    Day 3 part 2 works

--- src/Day3/hardware.ml
+++ src/Day3/hardware.ml
@@ -5,6 +5,7 @@ module I = struct
   type 'a t =
     { clock : 'a
     ; clear : 'a
+    ; new_input : 'a
     ; number : 'a[@bits 4]
     ; remaining : 'a[@bits 8] (* number of characters remaining after current *)
     ; valid : 'a
@@ -15,7 +16,7 @@ end
 module O = struct
   type 'a t =
     { ready : 'a (* whether output is valid/ready to receive input *)
-    ; result_bcd : 'a[@bits 48] (* 12 digits in binary *)
+    ; count : 'a[@bits 64]
     }
   [@@deriving hardcaml]
 end
@@ -43,9 +44,8 @@ module Processor = struct
       ; valid: 'a           (* pass through *)
       ; number: 'a[@bits 4] (* pass through *)
       ; min_index: 'a[@bits 4] (* pass through *)
-      ; zeroing: 'a (* whether this number is now zeroing *)
-      ; ready: 'a   (* whether the output is ready*)
-      }
+      ; zeroing: 'a (* whether this number is now zeroing *)     
+    }
     [@@deriving hardcaml]
   end
 
@@ -68,73 +68,98 @@ module Processor = struct
         i.zeroing |: (input_active &: (i.number >: value_reg))
       )
     in
-    let ready_reg = reg_fb r_sync ~enable:vdd ~width:1 ~f:(fun _ -> ~:valid_reg) in
     { O.value = value_reg
     ; O.valid = valid_reg
     ; O.number = number_reg
     ; O.min_index = min_index_reg
     ; O.zeroing = zeroing_reg
-    ; O.ready = ready_reg
     }
   ;;
 end
 
+let bcd_file_to_int_signal values n =
+  let rec pow_of_10 n =
+    if n = 0 then 1 else 10 * pow_of_10 (n - 1)
+  in
+  let segments = List.mapi (fun i bcd ->
+    uresize (bcd *: of_int ~width:(n * 4) (pow_of_10 i)) (n * 4)
+  ) values
+  in
+  List.fold_left (+:) (of_int ~width:(n * 4) 0) segments
+;;
+
+let construct_processor_file n ~clock ~clear ~new_input ~valid ~number ~remaining =
+  List.fold_left (fun acc idx ->
+  let proc_i = Processor.create {
+    index = of_int ~width:4 idx;
+    clock;
+    clear = clear |: new_input;
+    valid = if idx == 0 then valid else (List.hd acc).Processor.O.valid;
+    number = if idx == 0 then number else (List.hd acc).Processor.O.number;
+    min_index = if idx == 0 then mux2
+        (remaining >:. 11) (of_int ~width:4 0) ((of_int ~width:4 11) -: select remaining 3 0)
+      else
+        (List.hd acc).Processor.O.min_index;
+    zeroing = if idx == 0 then gnd else (List.hd acc).Processor.O.zeroing;
+  } in proc_i :: acc
+  ) [] (List.init n (fun i -> i))
+;;
+
 (* streaming processor *)
 (* inputs passed between *)
 let create (i : _ I.t) =
-    let processor_file =
-      List.fold_left (fun acc idx ->
-        let proc_i = Processor.create {
-          index = of_int ~width:4 idx;
-          clock = i.clock;
-          clear = i.clear;
-          valid = if idx == 0 then i.valid else (List.hd acc).Processor.O.valid;
-          number = if idx == 0 then i.number else (List.hd acc).Processor.O.number;
-          min_index = if idx == 0 then mux2
-              (i.remaining >:. 11) (of_int ~width:4 0) ((of_int ~width:4 11) -: select i.remaining 3 0)
-            else
-              (List.hd acc).Processor.O.min_index;
-          zeroing = if idx == 0 then gnd else (List.hd acc).Processor.O.zeroing;
-        } in
-        proc_i :: acc
-      ) [] (List.init 12 (fun i -> i))
+    let processor_file = construct_processor_file 12
+      ~clock:i.clock
+      ~clear:i.clear
+      ~new_input:i.new_input
+      ~valid:i.valid
+      ~number:i.number
+      ~remaining:i.remaining
     in
-    {
-      O.ready = List.fold_left (fun acc p -> acc &: p.Processor.O.ready) vdd processor_file;
-      O.result_bcd = Signal.concat_msb (
-        List.map (fun p -> p.Processor.O.value) (List.rev processor_file)
+    let ready_signal = ~: (List.fold_left (fun acc p -> acc |: p.Processor.O.valid) gnd processor_file) in
+    let result_signal = bcd_file_to_int_signal (List.map (fun p -> p.Processor.O.value) processor_file) 12 in
+    let ready_reg = reg_fb (Reg_spec.create ~clock:i.clock ~clear:i.clear () ) ~enable:vdd ~width:1
+      ~f:(fun _ -> ready_signal)
+    in
+    let rising_edge = ready_signal &: ~:ready_reg in
+    let count_reg = reg_fb (Reg_spec.create ~clock:i.clock ~clear:i.clear () ) ~enable:vdd ~width:64
+      ~f:(fun v -> mux2
+        rising_edge
+        (v +: uresize result_signal 64)
+        v
       )
+    in
+    { O.ready = ready_reg
+    ; O.count = count_reg
     }
 ;;
 
 module Simulator = Cyclesim.With_interface(I)(O)
 
 let testbench input verbose =
-  let rec bcd_to_int n =
-    if n = 0 then 0
-    else (n land 0xF) + (10 * bcd_to_int (n lsr 4))
-  in
   let cycle_count = ref 0 in
   let sim = Simulator.create create in
   let inputs : _ I.t = Cyclesim.inputs sim in
   let outputs : _ O.t = Cyclesim.outputs sim in
 
-  (* Reset simulation *)
-  inputs.remaining := Bits.of_int ~width:8 0;
-  inputs.number := Bits.of_int ~width:4 0;
-  inputs.clear := Bits.vdd;
-  inputs.valid := Bits.gnd;
-  Cyclesim.cycle sim;
-
   let stream ~number =
+    (* indicate new input *)
+    inputs.remaining := Bits.of_int ~width:8 0;
+    inputs.number := Bits.of_int ~width:4 0;
+    inputs.clear := Bits.gnd;
+    inputs.valid := Bits.gnd;
+    inputs.new_input := Bits.vdd;
+    cycle_count := !cycle_count + 1;
+    Cyclesim.cycle sim;
+
     String.iteri (fun i c ->
-      (* provide input *)
       let digit = Char.code c - Char.code '0' in
       let remaining = String.length number - i - 1 in
       inputs.remaining := Bits.of_int ~width:8 remaining;
       inputs.number := Bits.of_int ~width:4 digit;
       inputs.clear := Bits.gnd;
       inputs.valid := Bits.vdd;
+      inputs.new_input := Bits.gnd;
       cycle_count := !cycle_count + 1;
       Cyclesim.cycle sim;
       (* Stdio.printf "result=%d\n" (bcd_to_int (Bits.to_int !(outputs.result_bcd))); *)
@@ -144,37 +169,34 @@ let testbench input verbose =
       inputs.number := Bits.of_int ~width:4 0;
       inputs.clear := Bits.gnd;
       inputs.valid := Bits.gnd;
+      inputs.new_input := Bits.gnd;
       cycle_count := !cycle_count + 1;
       Cyclesim.cycle sim;
       (* Stdio.printf "result=%d\n" (bcd_to_int (Bits.to_int !(outputs.result_bcd))); *)
     done;
     if verbose then
-      Stdio.printf "count=%d\n" (bcd_to_int (Bits.to_int !(outputs.result_bcd)));
+      Stdio.printf "count=%d\n" (Bits.to_int !(outputs.count));
   in
   List.iter (fun line -> stream ~number:line) input;
+  Stdio.printf "part2_count=%d\n" (Bits.to_int !(outputs.count));
   Stdio.printf "Total cycles: %d\n" !cycle_count
 ;;
 
 let test_input = [
   "987654321111111";
-  "111111111111111"
+  "811111111111119";
+  "234234234234278";
+  "818181911112111"
 ]
 
 let%expect_test "test small numbers" =
     (* Construct the simulation and get its input and output ports. *)
     testbench test_input true;
     [%expect {|
-      count=0
-      count=33
-      count=132
-      count=1142
-      count=1188513027
-      count=1188735249
-      count=1188735249
-      count=1189181695
-      count=1227775554
-      count=1227775554
-      count=1227775554
-      count=1227775554
-      Total cycles: 575
+      count=987654321111
+      count=1798765432230
+      count=2232999666508
+      count=3121910778619
+      part2_count=3121910778619
+      Total cycles: 116
       |}]
--- src/Day3/main.ml
+++ src/Day3/main.ml
@@ -3,19 +3,13 @@ open Hardware_day3.Hardware
 
 module Simulator = Cyclesim.With_interface(I)(O)
 
-(* let parse_line line = (* 1-5,8-10 -> [(1, 5); (8, 10)] *)
-  List.map (fun s -> 
-    String.split_on_char '-' s |> function
-      | [l; r] -> (int_of_string l, int_of_string r)
-      | _ -> failwith "Invalid line format"
-  ) (String.split_on_char ',' line)
+let parse_line line =
+  String.trim line
 ;;
 
-Stdio.printf "part1_ideal=19605500130, part2_ideal=36862281418\n"
-let file = "inputs/day2.txt"
+Stdio.printf "part1_ideal=16887, part2_ideal=167302518850275\n"
+let file = "inputs/day3.txt"
 
-let content = In_channel.with_open_text file In_channel.input_line
-(* let () = List.iter (fun p -> Printf.printf "%d-%d\n" (fst p) (snd p)) (parse_line (Option.get content)) *)
-let () = testbench (parse_line (Option.get content)) false *)
-
-let () = Stdio.printf "Day 3 simulation\n"
+let content = In_channel.with_open_text file In_channel.input_lines
+let () = testbench (List.map parse_line content) false
+(* let () = List.iter (fun line -> Stdio.printf "line=%s\n" (parse_line line)) content *)
